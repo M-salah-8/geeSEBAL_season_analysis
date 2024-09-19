36d38c2f6533c55a0d501cd55d948c2129a61501
M Salah <mohammedalmak98@gmail.com>
2024-09-19 06:21:28 +0200

    update request

--- products/ETa/sebal/local_sebal/download_meteorology.py
+++ products/ETa/sebal/local_sebal/download_meteorology.py
@@ -6,41 +6,45 @@ import os
 import cdsapi
 
 def download_era5_re_hourly(tif_dr, date):
-    previous_day = date - datetime.timedelta(days=1)
-    year = date.year.__str__()
-    months = [date.month.__str__().zfill(2)]
-    days = [previous_day.day.__str__().zfill(2), date.day.__str__().zfill(2)]
-    
-    image_dr = os.path.split(tif_dr)[0]
-    image_dr = os.path.split(image_dr)[0]
-    src = rasterio.open(tif_dr)
-    bbox = rasterio.warp.transform_bounds(src.crs, 'EPSG:4326', *src.bounds)
-    bounds = int(bbox[3]+1), int(bbox[0]), int(bbox[1]), int(bbox[2]+1)
+    image_dr = os.path.dirname(tif_dr)
+    image_dr = os.path.dirname(image_dr)
+    era5_file = os.path.join(image_dr, 'era5-hourly.grib')
+    if os.path.exists(era5_file):
+        print("-----------era5 data already exists-----------")
+    else:
+        print("-----------downloading era5 data-----------")
+        previous_day = date - datetime.timedelta(days=1)
+        year = [date.year.__str__()]
+        months = [date.month.__str__().zfill(2)]
+        days = [previous_day.day.__str__().zfill(2), date.day.__str__().zfill(2)]
+        src = rasterio.open(tif_dr)
+        bbox = rasterio.warp.transform_bounds(src.crs, 'EPSG:4326', *src.bounds)
+        bounds = int(bbox[3]+1), int(bbox[0]), int(bbox[1]), int(bbox[2]+1)
 
-    c = cdsapi.Client()
-
-    c.retrieve(
-        'reanalysis-era5-single-levels',
-        {
-            'product_type': 'reanalysis',
-            'format': 'grib',
-            'variable': [
-                '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
-                '2m_temperature', 'surface_solar_radiation_downwards',
-            ],
+        dataset = "reanalysis-era5-single-levels"
+        request = {
+            'product_type': ['reanalysis'],
             'year': year,
             'month': months,
             'day': days,
             'time': [
-                '00:00', '01:00', '02:00',
-                '03:00', '04:00', '05:00',
-                '06:00', '07:00', '08:00',
-                '09:00', '10:00', '11:00',
-                '12:00', '13:00', '14:00',
-                '15:00', '16:00', '17:00',
-                '18:00', '19:00', '20:00',
-                '21:00', '22:00', '23:00',
-            ],
-            'area': bounds,
-        },
-        os.path.join(image_dr, 'era5-hourly.grib'))
+                    '00:00', '01:00', '02:00',
+                    '03:00', '04:00', '05:00',
+                    '06:00', '07:00', '08:00',
+                    '09:00', '10:00', '11:00',
+                    '12:00', '13:00', '14:00',
+                    '15:00', '16:00', '17:00',
+                    '18:00', '19:00', '20:00',
+                    '21:00', '22:00', '23:00',
+                ],
+            'data_format': 'grib',
+            'download_format': 'unarchived',
+            'variable': [
+                    '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
+                    '2m_temperature', 'surface_solar_radiation_downwards',
+                ],
+            'area': bounds
+        }
+        client = cdsapi.Client()
+        client.retrieve(dataset, request, era5_file)
+        print("-----------downloaded-----------")
